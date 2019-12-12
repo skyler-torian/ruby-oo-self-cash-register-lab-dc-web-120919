e94ba0392f0552f99fd0b585051a13572161e61a
github username <github email address>
2019-12-12 09:59:46 -0500

    pause

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
 
+    attr_accessor :total, :discount
+
+    def initialize(total=0, discount = 20)
+        @total = total
+        @discount = discount
+        
+    end
+
+    def add_item
+        
+
+
+end
